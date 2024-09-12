fb4c5ab9ce4b830e00b1bd057856385bd96839ed
MRoss <marianoross27@gmail.com>
2024-09-12 15:34:41 -0600

    arreglo circulo otra vez

--- Act_1/paint.py
+++ Act_1/paint.py
@@ -35,7 +35,7 @@ def square(start, end):
 
     end_fill()
 
-def circle(start, end):
+def draw_circle(start, end):
     """Draw circle from start to end."""
     up()
     goto(start.x, start.y)
@@ -111,7 +111,7 @@ onkey(lambda: color('red'), 'R')
 onkey(lambda: color('purple'), 'P')  # New color
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', draw_circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
