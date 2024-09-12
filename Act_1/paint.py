bfb1113e4d07e602d39a931fa89e7c2c4fd05b50
MRoss <marianoross27@gmail.com>
2024-09-12 15:05:29 -0600

    completar circulo

--- Act_1/paint.py
+++ Act_1/paint.py
@@ -35,11 +35,18 @@ def square(start, end):
 
     end_fill()
 
-
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
-
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    # Draw circle based on radius calculated from start to end
+    radius = ((end.x - start.x) ** 2 + (end.y - start.y) ** 2) ** 0.5
+    turtle_circle(radius)
+    
+    end_fill()
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
