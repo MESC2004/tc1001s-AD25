b53a87007a0a407eed2f20ceae0371795db826da
MRoss <marianoross27@gmail.com>
2024-09-12 15:23:03 -0600

    color nuevo

--- Act_1/paint.py
+++ Act_1/paint.py
@@ -98,7 +98,6 @@ def store(key, value):
     """Store value in state at key."""
     state[key] = value
 
-
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
@@ -109,6 +108,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('purple'), 'P')  # New color
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
