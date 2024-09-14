34c857d7be2a38a8304b81dcdd3c21632f136aad
Miguel Soria <a01028033@tec.mx>
2024-09-13 21:30:56 -0600

    Added letters and symbols replacing the numbers

--- Act_5/memory.py
+++ Act_5/memory.py
@@ -15,7 +15,9 @@ from turtle import *
 from freegames import path
 
 car = path('car.gif')
-tiles = list(range(32)) * 2
+tiles = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+ 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^'] * 2
+
 state = {'mark': None}
 hide = [True] * 64
 tap_no = 0 # initialization
